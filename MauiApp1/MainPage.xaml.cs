d286ab0ba1266471ec29edb7ba98e366dd1f4212
Iliza <iliza@users.noreply.github.com>
2022-09-29 00:58:19 -0400

    Add project files.

---
+++ MauiApp1/MainPage.xaml.cs
@@ -0,0 +1,44 @@
+﻿using Plugin.Maui.Audio;
+
+namespace MauiApp1;
+
+public partial class MainPage : ContentPage
+{
+    private readonly IAudioManager audioManager;
+    int playerRandomNum;
+    Random random = new Random();
+    int count = 0;
+    public MainPage(IAudioManager audioManager)
+	{
+		InitializeComponent();
+        this.audioManager = audioManager;
+	}
+
+	private async void OnCounterClicked(object sender, EventArgs e)
+	{
+		count++;       
+        // Generate a random number
+        playerRandomNum = random.Next(1, 7);
+        string v = $" {playerRandomNum} ";
+        Images.Source = ImageSource.FromFile($"dice{playerRandomNum}.png");
+
+        // Text-speech number of total dice rolled
+        TextToSpeech.Default.SpeakAsync($"You rolled {playerRandomNum}");
+
+        // Play a sound of the dice rolling
+        var playSound = audioManager.CreatePlayer(await FileSystem.OpenAppPackageFileAsync("foley_2_dice_throw_on_table.mp3"));
+        playSound.Play();
+
+        // Display the number of total dice rolled
+        RollBtn.Text = $"You rolled  {count} times ";
+        SemanticScreenReader.Announce(RollBtn.Text);
+
+        // 
+        TimeSpan vibrationLength = TimeSpan.FromSeconds(3);
+        Vibration.Default.Vibrate(vibrationLength);
+        Vibration.Default.Cancel();
+
+    }
+
+}
+
---
+++ MauiApp1/MauiProgram.cs
@@ -0,0 +1,28 @@
+﻿using Plugin.Maui.Audio;
+
+namespace MauiApp1;
+
+public static class MauiProgram
+{
+	public static MauiApp CreateMauiApp()
+	{
+		var builder = MauiApp.CreateBuilder();
+		builder
+			.UseMauiApp<App>()
+			.ConfigureFonts(fonts =>
+			{
+				fonts.AddFont("OpenSans-Regular.ttf", "OpenSansRegular");
+				fonts.AddFont("OpenSans-Semibold.ttf", "OpenSansSemibold");
+			});
+		builder.Services.AddSingleton(AudioManager.Current);
+		builder.Services.AddTransient<MainPage>();
+
+		return builder.Build();
+	}
+
+    
+}
+
+
+
+
