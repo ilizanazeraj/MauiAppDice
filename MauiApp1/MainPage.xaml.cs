cdea5f645b11f7054d40c23be5e09d4704bf0448
Iliza <iliza@users.noreply.github.com>
2022-10-02 23:11:34 -0400

    add vibration

--- MauiApp1/MainPage.xaml.cs
+++ MauiApp1/MainPage.xaml.cs
@@ -6,11 +6,9 @@ namespace MauiApp1;
  * Course: CMPSC 488
  * Author: Iliza Nazeraj 
  * Date Developed: 09/28/2022
- * Last Date Changed: 09/29/2022
- * Rev:1
+ * Last Date Changed: 10/02/2022
+ * Rev:3
  */
-
-
 
 
 public partial class MainPage : ContentPage
@@ -27,7 +25,8 @@ public partial class MainPage : ContentPage
 
 	private async void OnCounterClicked(object sender, EventArgs e)
 	{
-		count++;       
+		count++;    
+        
         // Generate a random number
         playerRandomNum = random.Next(1, 7);
         Images.Source = ImageSource.FromFile($"dice{playerRandomNum}.png");
@@ -39,11 +38,36 @@ public partial class MainPage : ContentPage
         var playSound = audioManager.CreatePlayer(await FileSystem.OpenAppPackageFileAsync("foley_2_dice_throw_on_table.mp3"));
         playSound.Play();
 
+        // Vibrate when the dice is rolling
+        try
+        {
+            Vibration.Default.Vibrate(TimeSpan.FromSeconds(3));
+        }
+        catch (Exception )
+        {
+            Console.WriteLine("{0} Exception caught.");
+        }
+
+
+        // Turn Flashlight ON
+        // Turn Flashlight On, wait one sec before turning back on
+        for (int i = 0; i < count; i++)
+        {
+            try
+            {
+                await Flashlight.Default.TurnOnAsync();
+                System.Threading.Thread.Sleep(1000);
+                await Flashlight.Default.TurnOffAsync();
+            }
+            catch (FeatureNotSupportedException)
+            {
+                // Handle not supported on device exception
+            }
+        }
+
         // Display the number of total dice rolled
         RollBtn.Text = $"You rolled  {count} times ";
         SemanticScreenReader.Announce(RollBtn.Text);
-
-        
 
     }
 
